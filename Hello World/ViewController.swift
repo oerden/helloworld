80e66d89e391ec144ab8a36f47843e3fef514753
Ozan Oğuz Erden <oerden@Ozans-MacBook-Pro.local>
2016-09-14 01:31:27 +0300

    deneme

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Ozan Oğuz Erden on 13/09/2016.
+//  Copyright © 2016 Oyunbozan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
